bdc892d9eabba20d7d811969602cda23a2bc8444
Felipe Barros <liperabarros12@gmail.com>
2024-09-30 21:57:12 -0300

    feat: adiciona limite de transações diárias

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os 
+from datetime import datetime
 
 os.system("cls")
 
@@ -14,17 +15,44 @@ menu = """
 """
 
 LIMITE_DE_SAQUE = 500
-LIMITE_QUANTIDADE_SAQUE = 3
-saldo = 100
-quantidade_de_saques = 0
-historico = []
+LIMITE_QUANTIDADE_TRANSACAO = 3
+saldo = 1000    
+historico = [{
+  "valor": 200,
+  "tipo": "Saque",
+  "data": datetime(2024, 9, 27, 12, 5)
+},{
+  "valor": 200,
+  "tipo": "Saque",
+  "data": datetime(2024, 9, 27, 11, 10)
+},{
+  "valor": 200,
+  "tipo": "Saque",
+  "data": datetime(2024, 9, 28, 2, 25)
+},{
+  "valor": 200,
+  "tipo": "Saque",
+  "data": datetime(2024, 9, 28, 11, 10)
+}]
+
+
+def formata_data(data):
+  data_formatada = data.strftime("%d/%m/%Y %H:%M")
+  return data_formatada
 
 def formatar_moeda(valor):
   return f"R$ {valor:,.2f}"
 
+def quantidades_transacoes_diaria():
+  global historico
+
+  hoje = datetime.now().date()
+  contador = sum(1 for item in historico if item.get("data").date() == hoje)
+  return contador
+
 def realizar_saque():
   global saldo
-  global quantidade_de_saques
+  quantidade_de_transacao = quantidades_transacoes_diaria()
 
   print("\nVocê escolheu sacar.")
   valor_digitado = int(input("Digite o valor: "))
@@ -32,35 +60,48 @@ def realizar_saque():
     print("Você não possui saldo suficiente.")
   elif valor_digitado > LIMITE_DE_SAQUE:
     print(f"Seu limite por saque é de {formatar_moeda(LIMITE_DE_SAQUE)}.")
-  elif quantidade_de_saques >= LIMITE_QUANTIDADE_SAQUE:
-    print(f"Seu limite de saque diário é de {LIMITE_QUANTIDADE_SAQUE} saques.") 
+  elif quantidade_de_transacao >= LIMITE_QUANTIDADE_TRANSACAO:
+    print(f"Seu limite de transações é de {LIMITE_QUANTIDADE_TRANSACAO}.") 
   else:
     saldo = saldo - valor_digitado
-    quantidade_de_saques = quantidade_de_saques + 1
     print(f"Saque realizado com sucesso.\nSeu saldo atual é: {formatar_moeda(saldo)}")
-    historico.append(f"Saque: {formatar_moeda(valor_digitado)}")
+    hoje = datetime.now()
+    historico.append({
+      "valor": valor_digitado,
+      "tipo": "Saque",
+      "data": hoje
+    })
+    
 
 def realizar_deposito():
   global saldo
+  quantidade_de_transacao = quantidades_transacoes_diaria()
 
   print("\nVocê escolheu depositar.")
   valor_digitado = int(input("Digite o valor que deseja depositar: "))
   if valor_digitado <= 0:
     print("Digite um valor positivo.")
+  elif quantidade_de_transacao >= LIMITE_QUANTIDADE_TRANSACAO:
+    print(f"Seu limite de transações é de {LIMITE_QUANTIDADE_TRANSACAO}.")
   else:
     saldo = saldo + valor_digitado
     print(f"Depósito realizado com sucesso.\nSeu saldo atual é: {formatar_moeda(saldo)}")
-    historico.append(f"Depósito: {formatar_moeda(valor_digitado)}")
+    hoje = datetime.now()
+    historico.append({
+      "valor": valor_digitado,
+      "tipo": "Depósito",
+      "data": hoje
+    })
 
 def apresentar_extrato():
   print("""    Extrato da conta:\n""")
   if not historico:
-    print("""    Não foram realizadas movimentações.\n""")
+    print("""    Não foram realizadas movimentações.\n""")  
   else: 
     for item in historico:
-      print(f"""    {item}""")
-  print(f"""    Saldo atual: {formatar_moeda(saldo)}""")
-
+      print(f"""   {item['tipo']}: {formatar_moeda(item['valor'])} .......... {formata_data(item['data'])}""")
+  print(f"""   Saldo atual: {formatar_moeda(saldo)}""")
+ 
 while True:
   print(menu)
   opcao = input("Digite uma opção: ")
